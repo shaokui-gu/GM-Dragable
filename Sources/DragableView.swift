ebe72f7ab7471d3f69fb59672dc9def19f2b5c84
gsk <gsk@kroknow.com>
2022-01-27 12:13:16 +0800

    更新手势识别强度

--- Sources/DragableView.swift
+++ Sources/DragableView.swift
@@ -365,7 +365,7 @@ class DragableView: UIView, UIGestureRecognizerDelegate {
             let position = gestureRecognizer.location(in: self)
             var frame = indicatorView.frame
             let velocity = pan.velocity(in: self)
-            guard abs(velocity.y) >= 20 else {
+            guard abs(velocity.y) >= 30 else {
                 return false
             }
             if velocity.y > 0 {
